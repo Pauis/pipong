eb2e398f0687fc29546c44caad89c33d2b5204de
Dong-jun Lim <ciqete@gmail.com>
2015-09-19 12:43:58 +0900

    New constant.h

---
+++ constant.h
@@ -0,0 +1,24 @@
+#ifndef __CONSTANT_H__
+#define __CONSTANT_H__
+
+/* main.c */
+#define P1SWITCH1 0
+#define P1SWITCH2 1
+#define P1LED1    4
+#define P1LED2    5
+#define P2SWITCH1 6
+#define P2SWITCH2 7
+#define P2LED1    12
+#define P2LED2    13
+
+#define DELAYTIME 25
+
+/* asciiartcommon.c */
+#define CHARTXT   1
+#define NUMTXT    2
+#define BLANK     3
+
+#define LENGTH    11
+#define WIDTH     7
+
+#endif
