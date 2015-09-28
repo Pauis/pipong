8e62c8b5563bb54f457641f4105c0c251443c14c
Dong-jun Lim <ciqete@gmail.com>
2015-09-28 16:50:12 +0900

    ponggpio.h setting

--- ponggpio.h
+++ ponggpio.h
@@ -1,15 +1,19 @@
-#ifndef __CONSTANT_H__
-#define __CONSTANT_H__
+#ifndef __PONGGPIO_H__
+#define __PONGGPIO_H__
 
-#define P1SWITCH1 0
-#define P1SWITCH2 1
-#define P1LED1    4
-#define P1LED2    5
-#define P2SWITCH1 6
-#define P2SWITCH2 7
-#define P2LED1    12
-#define P2LED2    13
-
-#define DELAYTIME 25
+namespace pong
+{
+	class PGpio
+	{
+	public:
+		P1SWITCH1 = 0,
+		P1SWITCH2 = 1,
+		P1LED1    = 4,
+		P1LED2    = 5,
+		P2SWITCH1 = 6,
+		P2SWITCH2 = 7,
+		P2LED1    = 12,
+		P2LED2    = 13
+	}
 
 #endif
