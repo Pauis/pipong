087abaab1576df36657135e8f307bca10a8e60fd
Dong-jun Lim <ciqete@gmail.com>
2015-10-26 22:02:02 +0900

    Add mainautomation

---
+++ mainautomation.cpp
@@ -0,0 +1,7 @@
+#include "mainautomation.h"
+
+
+void MainAM::AMRectMove(SOut& sout, PRect& prect, int lr, int ud)
+{
+	
+}
---
+++ mainautomation.h
@@ -0,0 +1,16 @@
+#ifndef __MAINAUTOMATION_H__
+#define __MAINAUTOMATION_H__
+
+#include "sysio.h"
+#include "pongobj.h"
+
+using pong::sys::SOut;
+using pong::PRect;
+
+class MainAM
+{
+public:
+	static void AMRectMove(SOut& sout, PRect& prect, int lr, int ud);
+};
+
+#endif
